4b60d9092b1dd38e6eedd1482523a38e3d3a3ce5
Richard Armuelles <admin@kurai021.com>
2020-02-24 13:51:46 -0500

    build uglify

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,13 @@
 const {task,dest,src,series,parallel,start} = require('gulp');
 const browserify = require('gulp-browserify');
+const uglify = require("gulp-uglify");
 const connect = require("gulp-connect");
 const watch = require("gulp-watch");
 
 task('render', function(done) {
   src('./assets/js/main.js')
     .pipe(browserify({ transform: ['vueify', 'babelify', 'aliasify'] }))
+    .pipe(uglify())
     .pipe(dest('./assets/build'))
   done();
 })
@@ -18,7 +20,7 @@ task('connect', function() {
 });
 
 task('watch-and-reload', series('render', function() {
-  watch(['assets/templates/**',"assets/js/main.js"], function() {
+  watch(['assets/templates/**',"assets/js/main.js", "assets/css/**"], function() {
     start('render');
   }).pipe(connect.reload());
 }));
